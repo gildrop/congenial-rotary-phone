d56f10c7e6ec75a49ee421e94be335e1df0dddcc
gildrop <tpc.gildrop@gmail.com>
2025-07-13 18:40:25 +0800

    Update server.js

--- tag-server/server.js
+++ tag-server/server.js
@@ -1,41 +1,42 @@
-// tag-server/server.js
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+// server.js (no express)
+const http = require("http");
+const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 8080;
-const app = express();
-const server = http.createServer(app);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("WebSocket server is running.");
+});
+
 const wss = new WebSocket.Server({ server });
 
 let players = {};
 let nextPlayerId = 1;
 
-app.get('/', (req, res) => res.send("WebSocket server is running."));
-
-wss.on('connection', (ws) => {
+wss.on("connection", (ws) => {
   const id = nextPlayerId++;
   players[id] = { x: 100, y: 100 };
 
-  ws.send(JSON.stringify({ type: 'init', id, players }));
+  ws.send(JSON.stringify({ type: "init", id, players }));
 
-  ws.on('message', (data) => {
+  ws.on("message", (data) => {
     const msg = JSON.parse(data);
-    if (msg.type === 'move') {
+    if (msg.type === "move") {
       players[id] = { x: msg.x, y: msg.y };
-      broadcast({ type: 'update', id, x: msg.x, y: msg.y });
+      broadcast({ type: "update", id, x: msg.x, y: msg.y });
     }
   });
 
-  ws.on('close', () => {
+  ws.on("close", () => {
     delete players[id];
-    broadcast({ type: 'remove', id });
+    broadcast({ type: "remove", id });
   });
 });
 
 function broadcast(msg) {
   const json = JSON.stringify(msg);
-  wss.clients.forEach(client => {
+  wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(json);
     }
