6bf5984078b5b17687c4e6d88c296e05a4955cd7
gildrop <tpc.gildrop@gmail.com>
2025-07-13 18:06:55 +0800

    Update server.js
    Simple Multiplayer Game Setup

--- tag-server/server.js
+++ tag-server/server.js
@@ -1,55 +1,41 @@
 // tag-server/server.js
-const WebSocket = require("ws");
-const server = new WebSocket.Server({ port: process.env.PORT || 8080 });
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
 
 let players = {};
-let next_id = 1;
+let nextPlayerId = 1;
 
-function broadcast_except(sender, data) {
-  const msg = JSON.stringify(data);
-  for (const id in players) {
-    const p = players[id];
-    if (p.socket !== sender && p.socket.readyState === WebSocket.OPEN) {
-      p.socket.send(msg);
-    }
-  }
-}
+server.on('connection', (socket) => {
+  const id = nextPlayerId++;
+  players[id] = { x: 100, y: 100 };
 
-server.on("connection", (ws) => {
-  const id = next_id++;
-  const x = 100 + Math.floor(Math.random() * 400);
-  const y = 200;
-  players[id] = { id, x, y, socket: ws };
+  socket.send(JSON.stringify({ type: 'init', id, players }));
 
-  ws.send(JSON.stringify({ type: "assign_id", id }));
+  socket.on('message', (data) => {
+    const msg = JSON.parse(data);
 
-  ws.send(JSON.stringify({
-    type: "player_list",
-    players: Object.values(players).filter(p => p.id !== id).map(p => ({
-      id: p.id,
-      x: p.x,
-      y: p.y
-    }))
-  }));
-
-  broadcast_except(ws, {
-    type: "player_joined",
-    id,
-    x,
-    y
-  });
-
-  ws.on("message", (msg) => {
-    const data = JSON.parse(msg);
-    if (data.type === "position" && players[id]) {
-      players[id].x = data.x;
-      players[id].y = data.y;
-      broadcast_except(ws, { type: "position", id, x: data.x, y: data.y });
+    if (msg.type === 'move') {
+      if (players[id]) {
+        players[id].x = msg.x;
+        players[id].y = msg.y;
+        broadcast({ type: 'update', id, x: msg.x, y: msg.y });
+      }
     }
   });
 
-  ws.on("close", () => {
+  socket.on('close', () => {
     delete players[id];
-    broadcast_except(ws, { type: "player_left", id });
+    broadcast({ type: 'remove', id });
   });
 });
+
+function broadcast(msg) {
+  const json = JSON.stringify(msg);
+  server.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(json);
+    }
+  });
+}
+
+console.log('WebSocket server running on ws://localhost:8080');
