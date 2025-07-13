caad2999eb092862909f1709476de05f94498669
gildrop <tpc.gildrop@gmail.com>
2025-07-13 12:53:43 +0800

    Update server.js

--- tag-server/server.js
+++ tag-server/server.js
@@ -3,22 +3,25 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 let players = [];
+let idCounter = 1;
 
 server.on('connection', (ws) => {
-  console.log("New player connected");
+  const playerId = idCounter++;
+  players.push({ id: playerId, socket: ws });
 
-  players.push(ws);
+  // Send player ID to the client
+  ws.send(JSON.stringify({ type: "assign_id", id: playerId }));
 
   ws.on('message', (msg) => {
-    // Broadcast to all players except sender
+    // Forward message to other player(s)
     players.forEach(p => {
-      if (p !== ws && p.readyState === WebSocket.OPEN) {
-        p.send(msg);
+      if (p.socket !== ws && p.socket.readyState === WebSocket.OPEN) {
+        p.socket.send(msg);
       }
     });
   });
 
   ws.on('close', () => {
-    players = players.filter(p => p !== ws);
+    players = players.filter(p => p.socket !== ws);
   });
 });
